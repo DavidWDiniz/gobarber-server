834976075542aa48dc7b8a6887dff0c71caf9080
david <davidwdiniz@gmail.com>
2020-09-01 10:13:35 -0300

    Início back-end { tratamento de exceções }

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -18,21 +18,19 @@ appointmentsRouter.get("/", async (request, response) => {
 
 
 appointmentsRouter.post("/", async (request, response) => {
-    try {
-        const {provider_id, date} = request.body;
 
-        const parsedDate = parseISO(date);
+    const {provider_id, date} = request.body;
 
-        const createAppointment = new CreateAppointmentService();
-        const appointment = await createAppointment.execute({
-            date: parsedDate,
-            provider_id,
-        });
+    const parsedDate = parseISO(date);
 
-        return response.json(appointment);
-    } catch (err) {
-        return response.status(400).json({Error: err.message});
-    }
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+        date: parsedDate,
+        provider_id,
+    });
+
+    return response.json(appointment);
 });
 
 export default appointmentsRouter;
