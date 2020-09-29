cd609d2071c867d624572b326370facfa0a4e4be
david <davidwdiniz@gmail.com>
2020-09-29 11:55:43 -0300

    Validando dados

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express, {NextFunction, Request, Response} from "express";
+import {errors} from "celebrate";
 import cors from "cors";
 import "express-async-errors";
 import routes from "./routes";
@@ -15,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
     if (err instanceof AppError) {
