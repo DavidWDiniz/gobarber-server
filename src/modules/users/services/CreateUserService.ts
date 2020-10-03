95cbef95be1dd0bee3f7ef7ead5cb031fe664f6b
david <davidwdiniz@gmail.com>
2020-10-03 17:29:46 -0300

    Invalidando cache

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -5,6 +5,7 @@ import User from "../infra/typeorm/entities/User";
 import AppError from "../../../shared/errors/AppError";
 import IUsersRepository from "../repositories/IUsersRepository";
 import IHashProvider from "../providers/HashProvider/models/IHashProvider";
+import ICacheProvider from "../../../shared/container/providers/CacheProvider/models/ICacheProvider";
 
 interface Request {
     name: string;
@@ -19,7 +20,10 @@ class CreateUserService {
         private usersRepository: IUsersRepository,
 
         @inject("HashProvider")
-        private hashProvider: IHashProvider
+        private hashProvider: IHashProvider,
+
+        @inject("CacheProvider")
+        private cacheProvider: ICacheProvider,
     ) {}
     async execute({name, email, password}: Request): Promise<User> {
 
@@ -31,11 +35,14 @@ class CreateUserService {
 
         const hashedPassword = await this.hashProvider.generateHash(password);
 
-        return await this.usersRepository.create({
+        const user = await this.usersRepository.create({
             name,
             email,
             password: hashedPassword
         });
+        await this.cacheProvider.invalidatePrefix("providers-list");
+
+        return user;
     }
 }
 
